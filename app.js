e3be1ebcbd944ab193e2ebb2a846ab154f528bcc
naina <nanu02d8@gmail.com>
2020-11-01 22:34:41 +0530

    Add header and footer

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", (req,res) => {
+    res.render("home");
+})
 
 app.listen(3000, ()=>{
     console.log("Server has started on port 3000");
